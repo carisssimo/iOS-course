ef38947b0829a85f623c0ba496f5e28adbb0f357
carisssimo <simonecari@tiscali.it>
2025-02-07 11:00:00 +0100

    feat: add decimal logic for balance

---
+++ Bankey/Bankey/CurrencyFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyFormatter.swift
+//  Bankey
+//
+//  Created by simonecaria on 07/02/25.
+//
+
+import Foundation
---
+++ Bankey/Bankey/Decimal+Utils.swift
@@ -0,0 +1,8 @@
+//
+//  Decimal+Utils.swift
+//  Bankey
+//
+//  Created by simonecaria on 07/02/25.
+//
+
+import Foundation
